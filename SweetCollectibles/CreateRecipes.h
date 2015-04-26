81fdbce73b20b83d0ff64a1abe25c1f0fee6a116
Rohini Carlson <it-hogskolan@it-hogskolans-MacBook-Pro-96.local>
2015-04-26 12:15:27 +0200

    Adding functionality for creating recipes.

---
+++ SweetCollectibles/CreateRecipes.h
@@ -0,0 +1,13 @@
+//
+//  CreateRecipes.h
+//  SweetCollectibles
+//
+//  Created by it-högskolan on 2015-04-26.
+//  Copyright (c) 2015 it-h&#246;gskolan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CreateRecipes : NSObject
+
+@end
---
+++ SweetCollectibles/CreateRecipes.m
@@ -0,0 +1,13 @@
+//
+//  CreateRecipes.m
+//  SweetCollectibles
+//
+//  Created by it-högskolan on 2015-04-26.
+//  Copyright (c) 2015 it-h&#246;gskolan. All rights reserved.
+//
+
+#import "CreateRecipes.h"
+
+@implementation CreateRecipes
+
+@end
